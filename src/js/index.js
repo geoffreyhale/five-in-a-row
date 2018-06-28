108afee2494fcb4c973ad385a15265866b91ca3c
geoffreyhale <geoffreyhale@gmail.com>
2018-06-27 22:47:15 -0700

    add cellIsFIAR replaces checkBoardForFiveInARow logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,18 @@
 import * as ai from './ai.js';
+//@todo breakup the following into game logic vs DOM manipulation
 
 /**
  * Config
  */
-const boardWidth = 8;
-const boardHeight = 12;
+const BOARD_HEIGHT = 15;
+const BOARD_WIDTH = 15;
+const COLOR_WIN = 'lime';
+
+/**
+ * Globals
+ */
+let playerTurn = 1; //player 1 or 2 //@todo isn't really being used atm
+let gameover = false;
 
 /**
  * Initialize Board
@@ -30,40 +38,111 @@ const getBoardSymbol = (p) => {
 };
 
 let board = [];
-for (let i = 0; i < boardHeight; i++) {
+for (let i = 0; i < BOARD_HEIGHT; i++) {
     board[i] = [];
-    for (let j = 0; j < boardWidth; j++) {
+    for (let j = 0; j < BOARD_WIDTH; j++) {
         board[i][j] = 0;
     }
 }
 
-const boardCellElements = [];
-for (let i = 0; i < boardHeight; i++) {
-    boardCellElements[i] = [];
-    for (let j = 0; j < boardWidth; j++) {
-        const cellElement = document.createElement('td');
-        cellElement.setAttribute('data-i', i.toString());
-        cellElement.setAttribute('data-j', j.toString());
-        boardCellElements[i][j] = cellElement;
-    }
-}
+/**
+ * Cell Is (Part Of At Least) Five In A Row
+ *
+ * Strategy:
+ * 1. Count similar value consecutive adjacent cells in all 8 directions (starting North, cw)
+ * 2. Sum count totals for all 4 directions (starting North, cw)
+ */
+const cellIsFIAR = (board, i, j) => {
+    const minI = 0;
+    const maxI = board.length - 1;
+    const minJ = 0;
+    const maxJ = board[0].length - 1; // warning: assumes rectangular
 
-const boardElement = document.createElement('table');
-const boardElementBody = boardElement.appendChild(document.createElement('tbody'));
-for (let i = 0; i < boardHeight; i++) {
-    const boardRow = boardElementBody.appendChild(document.createElement('tr'));
-    for (let j = 0; j < boardWidth; j++) {
-        boardRow.appendChild(boardCellElements[i][j]);
-    }
-}
+    const val = board[i][j];
+    const count = {n:0, ne:0, e:0, se:0, s:0, sw:0, w:0, nw:0};
 
-const updateBoard = (i, j, playerTurn) => {
-    board[i][j] = playerTurn;
-    boardCellElements[i][j].innerText = getBoardSymbol(board[i][j]);
-    boardCellElements[i][j].style.color = 'black';
+    //North
+    for (let n = 0; n < 5; n++) {
+        if (i - n >= minI) {
+            if (board[i-n][j] == val) {
+                count.n++;
+            }
+        }
+    }
+    //Northeast
+    for (let n = 0; n < 5; n++) {
+        if (i - n >= minI && j + n <= maxJ) {
+            if (board[i-n][j+n] == val) {
+                count.ne++;
+            }
+        }
+    }
+    //East
+    for (let n = 0; n < 5; n++) {
+        if (j + n <= maxJ) {
+            if (board[i][j+n] == val) {
+                count.e++;
+            }
+        }
+    }
+    //Southeast
+    for (let n = 0; n < 5; n++) {
+        if (i + n <= maxI && j + n <= maxJ) {
+            if (board[i+n][j+n] == val) {
+                count.se++;
+            }
+        }
+    }
+    //South
+    for (let n = 0; n < 5; n++) {
+        if (i + n <= maxI) {
+            if (board[i+n][j] == val) {
+                count.s++;
+            }
+        }
+    }
+    //Southwest
+    for (let n = 0; n < 5; n++) {
+        if (i + n <= maxI && j - n >= minJ) {
+            if (board[i+n][j-n] == val) {
+                count.sw++;
+            }
+        }
+    }
+    //West
+    for (let n = 0; n < 5; n++) {
+        if (j - n >= minJ) {
+            if (board[i][j-n] == val) {
+                count.w++;
+            }
+        }
+    }
+    //Northwest
+    for (let n = 0; n < 5; n++) {
+        if (i - n >= minI && j - n >= minJ) {
+            if (board[i-n][j-n] == val) {
+                count.nw++;
+            }
+        }
+    }
+
+    return 5 <= count.n + count.s - 1
+        || 5 <= count.ne + count.sw - 1
+        || 5 <= count.e + count.w - 1
+        || 5 <= count.se + count.nw - 1
+        ;
 };
 
-let playerTurn = 1; //player 1 or 2
+const checkBoardForFiveInARow = (i, j) => {
+    i = parseInt(i);
+    j = parseInt(j);
+
+    if (cellIsFIAR(board, i, j)) {
+        boardCellElements[i][j].style.background = COLOR_WIN;
+        gameover = true;
+    }
+};
+
 const cellClick = (e) => {
     const i = e.srcElement.getAttribute('data-i');
     const j = e.srcElement.getAttribute('data-j');
@@ -72,15 +151,12 @@ const cellClick = (e) => {
         updateBoard(i, j, playerTurn);
         checkBoardForFiveInARow(i, j);
 
-        // if (playerTurn == 1) { playerTurn = 2 }
-        // else if (playerTurn == 2) { playerTurn = 1 }
-        /**
-         * @todo belongs in a game loop:
-         */
-        const next = ai.getNextFromBoard(board);
-        updateBoard(next[0],next[1],2);
-        checkBoardForFiveInARow(next[0],next[1]);
-        console.log(board);
+        //@todo shouldn't be here
+        if (!gameover) {
+            const next = ai.getNextFromBoard(board);
+            updateBoard(next[0],next[1],2);
+            checkBoardForFiveInARow(next[0],next[1]);
+        }
     }
 };
 const cellMouseover = (e) => {
@@ -101,72 +177,35 @@ const cellMouseout = (e) => {
     }
 };
 
-function hasConsecutive(arr, amount) {
-    var last = null;
-    var count = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] != last) {
-            last = arr[i];
-            count = 0;
-        }
-        count += 1;
-        if (amount <= count) {
-            return true;
-        }
+const boardCellElements = [];
+for (let i = 0; i < BOARD_HEIGHT; i++) {
+    boardCellElements[i] = [];
+    for (let j = 0; j < BOARD_WIDTH; j++) {
+        const cellElement = document.createElement('td');
+        cellElement.setAttribute('data-i', i.toString());
+        cellElement.setAttribute('data-j', j.toString());
+        cellElement.addEventListener("click", cellClick);
+        cellElement.addEventListener("mouseover", cellMouseover);
+        cellElement.addEventListener("mouseout", cellMouseout);
+        boardCellElements[i][j] = cellElement;
     }
-    return false;
 }
-const checkBoardForFiveInARow = (i, j) => {
-    i = parseInt(i);
-    j = parseInt(j);
-    const series = {
-        horizontal: [[i,j-4],[i,j-3],[i,j-2],[i,j-1],[i,j],[i,j+1],[i,j+2],[i,j+3],[i,j+4]],
-        vertical: [[i-4,j],[i-3,j],[i-2,j],[i-1,j],[i,j],[i+1,j],[i+2,j],[i+3,j],[i+4,j]],
-        nw: [[i-4,j-4],[i-3,j-3],[i-2,j-2],[i-1,j-1],[i,j],[i+1,j+1],[i+2,j+2],[i+3,j+3],[i+4,j+4]],
-        ne: [[i-4,j+4],[i-3,j+3],[i-2,j+2],[i-1,j+1],[i,j],[i+1,j-1],[i+2,j-2],[i+3,j-3],[i+4,j-4]],
-    };
-    for (let seriesName in series) {
-        const offBoardIndexesToRemoveFromSeries = [];
-        // find out of range indexes
-        for (let n = 0; n < series[seriesName].length; n++) {
-            if (
-                series[seriesName][n][0] < 0
-                || series[seriesName][n][0] > boardHeight - 1
-                || series[seriesName][n][1] < 0
-                || series[seriesName][n][1] > boardWidth - 1
-            ) {
-                offBoardIndexesToRemoveFromSeries.push(n);
-            }
-        }
-        // remove out of range indexes
-        for (let m = offBoardIndexesToRemoveFromSeries.length - 1; m >= 0; m--) {
-            series[seriesName].splice(offBoardIndexesToRemoveFromSeries[m],1);
-        }
-        const seriesBoardValues = [];
-        for (let p = 0; p < series[seriesName].length; p++) {
-            seriesBoardValues.push(
-                board[
-                    series[seriesName][p][0]
-                    ][
-                    series[seriesName][p][1]
-                    ]
-            );
-        }
-        console.log(seriesBoardValues);
-        if (hasConsecutive(seriesBoardValues, 5)) {
-            console.log('Five in a row!');
-            document.body.style.background = 'grey';
-        }
+
+const boardElement = document.createElement('table');
+const boardElementBody = boardElement.appendChild(document.createElement('tbody'));
+for (let i = 0; i < BOARD_HEIGHT; i++) {
+    const boardRow = boardElementBody.appendChild(document.createElement('tr'));
+    for (let j = 0; j < BOARD_WIDTH; j++) {
+        boardRow.appendChild(boardCellElements[i][j]);
     }
+}
+
+const updateBoard = (i, j, playerTurn) => {
+    board[i][j] = playerTurn;
+    boardCellElements[i][j].innerText = getBoardSymbol(board[i][j]);
+    boardCellElements[i][j].style.color = 'black';
 };
 
 window.onload = (() => {
     document.getElementById('game').appendChild(boardElement);
-
-    const tds = document.body.getElementsByTagName('td');
-    for (let i = 0; i < tds.length; i++) {
-        tds[i].addEventListener("click", cellClick);
-        tds[i].addEventListener("mouseover", cellMouseover);
-        tds[i].addEventListener("mouseout", cellMouseout);
-    }
 });
