ffcb14061d62a1192fe69202cf26e11a0ec46909
geoffreyhale <geoffreyhale@gmail.com>
2018-06-27 18:03:12 -0700

    click toggles xo

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 let nextClick = 'x';
+const getNextClick = () => {
+  nextClick = nextClick == 'x' ? 'o' : 'x';
+  return nextClick;
+};
 
 window.onload = (() => {
     const tds = document.body.getElementsByTagName('td');
     for (let i = 0; i < tds.length; i++) {
-        tds[i].onclick = function() { tds[i].innerHTML = 'x'; };
+        tds[i].onclick = function() {
+            tds[i].innerHTML = getNextClick();
+        };
     }
 });
